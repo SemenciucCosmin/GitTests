5c5e7be6563b79454dd109c2fe3929a7e92e2c64
Semenciuc Cosmin <cosmin.semenciuc@garmin.com>
2023-04-28 08:41:49 +0300

    Change 3

--- GitTestsCompose/app/src/main/java/com/example/gittestscompose/MainActivity.kt
+++ GitTestsCompose/app/src/main/java/com/example/gittestscompose/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : ComponentActivity() {
                     Greeting("Android")
                     //Change 1
                     //Change 2
+                    //Change 3
                 }
             }
         }
